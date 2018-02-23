0d21fe60554a98e4453e907f00b9f35fe38713f0
Bas de Ruiter <ruiter@nic.local>
2018-02-23 14:58:24 +0100

    orm perstated Product Entity to database!

---
+++ app/src/Product.php
@@ -0,0 +1,44 @@
+<?php
+// src/Product.php
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Example
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="Product")
+ *
+ */
+class Product
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=125)
+     *
+     * @var string
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/app/src"), $isDevMode, null, null, false);
+// or if you prefer XML
+//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config"), $isDevMode);
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'dbname'   => 'orm_db',
+    'user'     => 'root',
+    'password' => '',
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
