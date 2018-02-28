6fa2ecd82a987ab7d4415656663bb348d100b574
Bas de Ruiter <ruiter@nic.local>
2018-02-28 08:42:03 +0100

    changed some settings

--- cli-config.php
+++ cli-config.php
@@ -1,5 +1,11 @@
 <?php
-// cli-config.php
-require_once "bootstrap.php";
 
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+// replace with file to your own project bootstrap
+require_once 'bootstrap.php';
+
+// replace with mechanism to retrieve EntityManager in your app
+$entityManager = GetEntityManager();
+
+return ConsoleRunner::createHelperSet($entityManager);
